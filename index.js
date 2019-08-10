e2140f4ef28cd2ba463ba4098f3315eef8767adf
felixmemmott <51401315+felixmemmott@users.noreply.github.com>
2019-08-10 19:58:43 +0100

    Added generic error handling

--- index.js
+++ index.js
@@ -11,77 +11,81 @@ client.once('ready', () => {
 client.on('message', message => {
 
 
+  try {
+    //amazonsearch
+    if (message.content.startsWith(`${prefix}asearch`)){
+      console.log(message.author + " : " + message.content);
+      i = Math.floor((Math.random() * responsecount));
+      message.channel.send(tallyho[i]);
 
-  //amazonsearch
-  if (message.content.startsWith(`${prefix}asearch`)){
-    console.log(message.author + " : " + message.content);
-    i = Math.floor((Math.random() * responsecount));
-    message.channel.send(tallyho[i]);
+      item = (message.content).substring(9,((message.content).length)+ 1);
+      item_formatted = item.replace(/ /g,"%20");
+      message.channel.send("Amazon search results for:" + item);
+      message.channel.send("https://www.amazon.com/gp/search?ie=UTF8&tag=fragginfelix-20&linkCode=ur2&linkId=99fd2185ee30fec478facdf1fb76b17b&camp=1789&creative=9325&index=aps&keywords=" + item_formatted);
+    }
 
-    item = (message.content).substring(9,((message.content).length)+ 1);
-    item_formatted = item.replace(/ /g,"%20");
-    message.channel.send("Amazon search results for:" + item);
-    message.channel.send("https://www.amazon.com/gp/search?ie=UTF8&tag=fragginfelix-20&linkCode=ur2&linkId=99fd2185ee30fec478facdf1fb76b17b&camp=1789&creative=9325&index=aps&keywords=" + item_formatted);
-  }
+    //ebaysearch
+    if (message.content.startsWith(`${prefix}esearch`)){
+      console.log(message.author + " : " + message.content);
+      i = Math.floor((Math.random() * responsecount));
+      message.channel.send(tallyho[i]);
 
-  //ebaysearch
-  if (message.content.startsWith(`${prefix}esearch`)){
-    console.log(message.author + " : " + message.content);
-    i = Math.floor((Math.random() * responsecount));
-    message.channel.send(tallyho[i]);
+      item = (message.content).substring(9,((message.content).length)+ 1);
+      item_formatted = item.replace(/ /g,"%20");
+      message.channel.send("Ebay search results for:" + item);
+      message.channel.send("https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=" + item_formatted);
+    }
 
-    item = (message.content).substring(9,((message.content).length)+ 1);
-    item_formatted = item.replace(/ /g,"%20");
-    message.channel.send("Ebay search results for:" + item);
-    message.channel.send("https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=" + item_formatted);
-  }
+    //googlesearch
+    if (message.content.startsWith(`${prefix}gsearch`)){
+      console.log(message.author + " : " + message.content);
+      i = Math.floor((Math.random() * responsecount));
+      message.channel.send(tallyho[i]);
 
-  //googlesearch
-  if (message.content.startsWith(`${prefix}gsearch`)){
-    console.log(message.author + " : " + message.content);
-    i = Math.floor((Math.random() * responsecount));
-    message.channel.send(tallyho[i]);
+      item = (message.content).substring(9,((message.content).length)+ 1);
+      item_formatted = item.replace(/ /g,"%20");
+      message.channel.send("Google search results for:" + item);
+      message.channel.send("https://www.google.com/search?source=hp&ei=KvtOXev-C6iRlwTCx7L4Bg&q=" + item_formatted);
+    }
 
-    item = (message.content).substring(9,((message.content).length)+ 1);
-    item_formatted = item.replace(/ /g,"%20");
-    message.channel.send("Google search results for:" + item);
-    message.channel.send("https://www.google.com/search?source=hp&ei=KvtOXev-C6iRlwTCx7L4Bg&q=" + item_formatted);
-  }
+    //letmegooglethatforyou
+    if (message.content.startsWith(`${prefix}lmgtfy`)){
+      console.log(message.author + " : " + message.content);
+      i = Math.floor((Math.random() * responsecount));
+      message.channel.send(tallyho[i]);
 
-  //letmegooglethatforyou
-  if (message.content.startsWith(`${prefix}lmgtfy`)){
-    console.log(message.author + " : " + message.content);
-    i = Math.floor((Math.random() * responsecount));
-    message.channel.send(tallyho[i]);
+      item = (message.content).substring(8,((message.content).length)+ 1);
+      item_formatted = item.replace(/ /g,"%20");
+      message.channel.send("LMGTFY link for:" + item);
+      message.channel.send("https://lmgtfy.com/?q=" + item_formatted);
+    }
 
-    item = (message.content).substring(8,((message.content).length)+ 1);
-    item_formatted = item.replace(/ /g,"%20");
-    message.channel.send("LMGTFY link for:" + item);
-    message.channel.send("https://lmgtfy.com/?q=" + item_formatted);
-  }
+    //youtubesearch
+    if (message.content.startsWith(`${prefix}ysearch`)){
+      console.log(message.author + " : " + message.content);
+      i = Math.floor((Math.random() * responsecount));
+      message.channel.send(tallyho[i]);
 
-  //youtubesearch
-  if (message.content.startsWith(`${prefix}ysearch`)){
-    console.log(message.author + " : " + message.content);
-    i = Math.floor((Math.random() * responsecount));
-    message.channel.send(tallyho[i]);
+      item = (message.content).substring(9,((message.content).length)+ 1);
+      item_formatted = item.replace(/ /g,"%20");
+      message.channel.send("Youtube search results for:" + item);
+      message.channel.send("https://www.youtube.com/results?search_query=" + item_formatted);
+    }
 
-    item = (message.content).substring(9,((message.content).length)+ 1);
-    item_formatted = item.replace(/ /g,"%20");
-    message.channel.send("Youtube search results for:" + item);
-    message.channel.send("https://www.youtube.com/results?search_query=" + item_formatted);
-  }
+    //soundcloudsearch
+    if (message.content.startsWith(`${prefix}ssearch`)){
+      console.log(message.author + " : " + message.content);
+      i = Math.floor((Math.random() * responsecount));
+      message.channel.send(tallyho[i]);
 
-  //soundcloudsearch
-  if (message.content.startsWith(`${prefix}ssearch`)){
-    console.log(message.author + " : " + message.content);
-    i = Math.floor((Math.random() * responsecount));
-    message.channel.send(tallyho[i]);
+      item = (message.content).substring(9,((message.content).length)+ 1);
+      item_formatted = item.replace(/ /g,"%20");
+      message.channel.send("Soundcloud search results for:" + item);
+      message.channel.send("https://soundcloud.com/search?q=" + item_formatted);
+    }
 
-    item = (message.content).substring(9,((message.content).length)+ 1);
-    item_formatted = item.replace(/ /g,"%20");
-    message.channel.send("Soundcloud search results for:" + item);
-    message.channel.send("https://soundcloud.com/search?q=" + item_formatted);
+  } catch(err) {
+    message.channel.send("Oh crumbs, an unknown command!");
   }
 
 
