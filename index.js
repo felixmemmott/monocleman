175a0c3445c5c0e8a0ea6c68be6d369aa7af3bad
felixmemmott <51401315+felixmemmott@users.noreply.github.com>
2019-08-10 19:59:28 +0100

    Formatted context message

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on('message', message => {
 
       item = (message.content).substring(9,((message.content).length)+ 1);
       item_formatted = item.replace(/ /g,"%20");
-      message.channel.send("Amazon search results for:" + item);
+      message.channel.send("Amazon search results for: " + item);
       message.channel.send("https://www.amazon.com/gp/search?ie=UTF8&tag=fragginfelix-20&linkCode=ur2&linkId=99fd2185ee30fec478facdf1fb76b17b&camp=1789&creative=9325&index=aps&keywords=" + item_formatted);
     }
 
@@ -32,7 +32,7 @@ client.on('message', message => {
 
       item = (message.content).substring(9,((message.content).length)+ 1);
       item_formatted = item.replace(/ /g,"%20");
-      message.channel.send("Ebay search results for:" + item);
+      message.channel.send("Ebay search results for: " + item);
       message.channel.send("https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=" + item_formatted);
     }
 
@@ -44,7 +44,7 @@ client.on('message', message => {
 
       item = (message.content).substring(9,((message.content).length)+ 1);
       item_formatted = item.replace(/ /g,"%20");
-      message.channel.send("Google search results for:" + item);
+      message.channel.send("Google search results for: " + item);
       message.channel.send("https://www.google.com/search?source=hp&ei=KvtOXev-C6iRlwTCx7L4Bg&q=" + item_formatted);
     }
 
@@ -56,7 +56,7 @@ client.on('message', message => {
 
       item = (message.content).substring(8,((message.content).length)+ 1);
       item_formatted = item.replace(/ /g,"%20");
-      message.channel.send("LMGTFY link for:" + item);
+      message.channel.send("LMGTFY link for: " + item);
       message.channel.send("https://lmgtfy.com/?q=" + item_formatted);
     }
 
@@ -68,7 +68,7 @@ client.on('message', message => {
 
       item = (message.content).substring(9,((message.content).length)+ 1);
       item_formatted = item.replace(/ /g,"%20");
-      message.channel.send("Youtube search results for:" + item);
+      message.channel.send("Youtube search results for: " + item);
       message.channel.send("https://www.youtube.com/results?search_query=" + item_formatted);
     }
 
@@ -80,7 +80,7 @@ client.on('message', message => {
 
       item = (message.content).substring(9,((message.content).length)+ 1);
       item_formatted = item.replace(/ /g,"%20");
-      message.channel.send("Soundcloud search results for:" + item);
+      message.channel.send("Soundcloud search results for: " + item);
       message.channel.send("https://soundcloud.com/search?q=" + item_formatted);
     }
 
